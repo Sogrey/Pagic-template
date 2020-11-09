d392eb7431faa990f05dd0cf45eb38d6afdec96a
Sogrey <suzhaoheng2006@163.com>
2020-11-09 09:51:36 +0800

    'Publish'

--- pagic.config.tsx
+++ pagic.config.tsx
@@ -19,8 +19,8 @@ export default {
       target: '_blank',
       popover: (
         <>
-          <img src="./assets/wechat.jpg" width="256" style={{ marginRight: '1rem', verticalAlign: 'top' }} />
-          <img src="./assets/alipay.jpg" width="256" style={{ verticalAlign: 'top' }} />
+          <img src="./Pagic-template/assets/wechat.jpg" width="256" style={{ marginRight: '1rem', verticalAlign: 'top' }} />
+          <img src="./Pagic-template/assets/alipay.jpg" width="256" style={{ verticalAlign: 'top' }} />
         </>
       )
     }
